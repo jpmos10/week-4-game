a20024d4289ef544ec1b548f038a4fb7d3f9384f
Jason Mosley <jpmos10@users.noreply.github.com>
2016-08-27 16:57:55 -0500

    pushing incomplete assignment

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var character = [
+	{name: 'Original Starbuck', img: <img src='assets/images/orig_starbuck_1.jpg' alt='Original Starbuck'>, hp: 120, ap: 12, ca: 10},
+	{name: 'New Starbuck', img: <img src='assets/images/new_starbuck_2.jpg' alt='New Starbuck'>, hp: 100, ap: 8, ca: 6},
+	{name: 'Original Cylon', img: <img src='assets/images/cylon_2.jpg' alt='Original Cylon'>, hp: 150, ap: 15, ca: 13},
+	{name: 'New Cylon', img: <img src='assets/images/new_cylon.jpg' alt='New Cylon'>, hp: 180, ap: 6, ca: 4}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
